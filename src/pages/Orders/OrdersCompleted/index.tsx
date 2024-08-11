4439273976bcbb33c1bcdac47603c775d51eac0d
Kirill Dyatlov <kirillsoftware@proton.me>
2024-08-11 16:11:55 +0300

    update orders/completed page

--- src/pages/Orders/OrdersCompleted/index.tsx
+++ src/pages/Orders/OrdersCompleted/index.tsx
@@ -3,7 +3,6 @@ import ButtonChooseList from '@common/components/ButtonChooseList/index'
 import CardsSliderRelated from '@common/components/CardsSliderRelated/index'
 import Footer from '@common/components/Footer/Footer'
 import Header from '@common/components/Header/Header/index'
-import NavigationBarSelection from '@common/components/NavigationBarSelection/index'
 import ResponsiveLayoutTwo from '@common/components/ResponsiveLayoutTwo/index'
 import NavigationItem from '@common/components/navigation_history/NavigationItem/index'
 import BigInput from '@common/components/ui/BigInput/index'
@@ -17,14 +16,27 @@ import SizeBox from '@common/components/ui/SizeBox/index'
 import SwitchButton from '@common/components/ui/SwitchButton/index'
 import TextDotted from '@common/components/ui/TextDotted/index'
 import Typography from '@common/components/ui/Typography/Typography'
-import UserAvatar from '@common/components/ui/UserAvatar/index'
 import UserTopPageInfo from '@common/components/ui/UserTopPageInfo/index'
 import { fakeUserConstant } from '@common/models/user'
 import AppColor from '@common/styles/variables-static'
 import { TipsItem } from '@pages/Partnership/pages/PartnershipCompleted/index'
 import { DetailsDropdownItem } from '@pages/Partnership/pages/ProgressFreelancer/index'
 import styles from './style.module.scss'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import StepsStates from '@common/components/StepsStates/index'
+import StatesModel from '@common/models/orders/statesModel'
+import ManagersDropDown from '@pages/Partnership/pages/ProgressFreelancer/components/ManagerDropdown/index'
+import PartnersModel from '@common/models/partnership/partnersModel'
+import FreelancerProjectsModel from '@common/models/partnership/freelancesProjectsModel'
+import { SubscriptionList } from '@pages/Service/Service/components/Subscriptions/List/index'
+import MissionModal from '@pages/Service/ServiceProgress/components/MissionModal/index'
+
+const freelancerProjectModel = new FreelancerProjectsModel(
+  FreelancerProjectsModel.makeFakeData()
+)
+const freelancerFakeProject = freelancerProjectModel.findByLabel('Progress')
+const freelancerFakeProjectProgress =
+  freelancerProjectModel.getProgress('Progress')
 
 const OrdersCompleted = () => {
   const arrayHistory = [
@@ -35,17 +47,24 @@ const OrdersCompleted = () => {
   ]
   const title = 'Logo by sample in vector in maximum quality'
 
+  const [partnersModel, setPartnersModel] = useState<PartnersModel | null>(null)
+  const [partnersSelectedUser, setPartnersSelecteduser] =
+    useState<PartnerShip.Manager | null>(null)
+  const [showMissionModal, setShowMissionModal] = useState<boolean>(false)
+
   useEffect(() => {
     window.scrollTo({ top: 0 })
   }, [])
+
+  useEffect(() => {
+    setPartnersModel(new PartnersModel())
+  }, [])
+
   return (
     <div>
       <Header />
 
-      <NavigationBarSelection
-        allItemsProgress={['Service', 'Negotiation', 'Progress', 'Completed']}
-        currentItemProgress="Completed"
-      />
+      <StepsStates states={StatesModel.getAll()} currentState={'Completed'} />
 
       <div className={'wrapper_page'}>
         <PageDetails
@@ -55,14 +74,14 @@ const OrdersCompleted = () => {
           pageTitle={title}
         />
 
-        <DynamicPadding desktop="30px" mobile="20px" />
+        <DynamicPadding desktop="14px" mobile="14px" />
         <UserTopPageInfo user={fakeUserConstant} />
-        <DynamicPadding />
+        <DynamicPadding desktop='42px' mobile='20px' />
 
         <ResponsiveLayoutTwo
           gap="80px"
           item1MaxWidth="730px"
-          item2MaxWidth="390px"
+          item2MaxWidth="388px"
           orderItem1Desktop={0}
           orderItem1Mobile={1}
           orderItem2Desktop={1}
@@ -72,14 +91,14 @@ const OrdersCompleted = () => {
               <Typography variant="body3" fontWeight="500">
                 Review
               </Typography>
-              <DynamicPadding desktop="30px" mobile="20px" />
+              <DynamicPadding desktop="23px" mobile="20px" />
               <BigInput />
-              <DynamicPadding />
+              <DynamicPadding desktop='44px' mobile='20px' />
 
               <Typography variant="body3" fontWeight="500">
                 Warranty
               </Typography>
-              <DynamicPadding desktop="30px" mobile="20px" />
+              <DynamicPadding desktop="27px" mobile="20px" />
               <CenterShadowBox
                 elements={[
                   <div className="gap_10">
@@ -91,27 +110,36 @@ const OrdersCompleted = () => {
                       <Typography variant="body4">3 of 10 days</Typography>
                     </div>
                   </div>,
+                  <DynamicPadding desktop='17px' mobile='17px' />,
                   <PercentBar currentPercent={30} height="3px" />,
+                  <DynamicPadding desktop='20px' mobile='20px' />,
                   <div className="gap_5_wrap">
                     <Typography variant="body4">
                       You can upgrade your subscription to extend your warranty
                     </Typography>
-                    <MyButtonTransparentOrange onClick={() => {}}>
+                    <SizeBox width='10px' />
+                    <MyButtonTransparentOrange
+                      padding="7.5px 14px"
+                      fontWeight="500"
+                      textTransform="uppercase"
+                      onClick={() => {}}
+                    >
                       <AppColor.buy fill={AppColor.orange} />
                       upgrade now
                     </MyButtonTransparentOrange>
                   </div>,
                 ]}
-                gap="20px"
-                paddingBoxDesktop="22px 30px"
+                align="start"
+                gap="0"
+                paddingBoxDesktop="22px 30px 30px 30px"
                 paddingBoxMobile="15px"
               />
-              <DynamicPadding />
+              <DynamicPadding desktop='25px' mobile='20px' />
 
               <Typography variant="body3" fontWeight="500">
                 Tips
               </Typography>
-              <DynamicPadding desktop="30px" mobile="20px" />
+              <DynamicPadding desktop="25px" mobile="20px" />
 
               <TipsItem
                 description={'Start a new order project with'}
@@ -122,30 +150,17 @@ const OrdersCompleted = () => {
           }
           item2={
             <div style={{ width: '100%' }}>
-              <div
-                className={`flex_space_between box_shadow ${styles.user_wrappper}`}
-              >
-                <UserAvatar
-                  role="Freelancer"
-                  preventMobileNone={true}
-                  url={fakeUserConstant.image}
-                  name={fakeUserConstant.name}
-                  flag={<AppColor.UkraineFlagIcon />}
-                  active={true}
-                />
-
-                <div className="gap_10">
-                  <Typography variant="body5" color={AppColor.transparentBlack}>
-                    15 hr 59 min ago
-                  </Typography>
-                  <AppColor.chevronBottom
-                    fill={AppColor.text}
-                    width={'16px'}
-                    height={'8px'}
-                  />
-                </div>
-              </div>
-              <DynamicPadding desktop="30px" mobile="15px" />
+              <ManagersDropDown
+                selectedUser={partnersSelectedUser}
+                users={partnersModel && partnersModel.getAll()}
+                onUserCallback={(id: string) => {
+                  if (partnersModel) {
+                    const uuid = partnersModel.findByUuid(id)
+                    setPartnersSelecteduser(uuid)
+                  }
+                }}
+              />
+              <DynamicPadding desktop="25px" mobile="20px" />
               <div className="justify_center">
                 <Typography
                   variant="body4"
@@ -159,12 +174,11 @@ const OrdersCompleted = () => {
               <div className={`box_shadow ${styles.details_box}`}>
                 <DetailsDropdownItem
                   title="Fixed"
-                  text=" 
-                                Fab 27, 2023 23:40 - Fab 28, 2023 21:55"
+                  text="Fab 27, 2023 23:40 - Fab 28, 2023 21:55"
                   initState={true}
                   node={
                     <div>
-                      <DynamicPadding desktop="30px" mobile="15px" />
+                      <DynamicPadding desktop="20px" mobile="15px" />
                       <div className="flex_space_between">
                         <Typography
                           variant="body4"
@@ -176,13 +190,13 @@ const OrdersCompleted = () => {
                           22 hrs 15 min
                         </Typography>
                       </div>
-                      <DynamicPadding desktop="15px" mobile="5px" />
+                      <DynamicPadding desktop="9px" mobile="5px" />
                       <PercentBar
-                        currentPercent={100}
-                        color={AppColor.green}
+                        currentPercent={freelancerFakeProjectProgress}
+                        color={freelancerFakeProject.color}
                         height="5px"
                       />
-                      <DynamicPadding desktop="15px" mobile="5px" />
+                      <DynamicPadding desktop="10px" mobile="5px" />
                       <div className="flex_space_between">
                         <Typography
                           variant="body4"
@@ -193,48 +207,27 @@ const OrdersCompleted = () => {
                         <Typography
                           variant="body4"
                           fontWeight="500"
-                          color={AppColor.green}
+                          color={freelancerFakeProject.color}
                         >
-                          Completed
+                          {freelancerFakeProject.label}
                         </Typography>
                       </div>
 
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="25px" mobile="20px" />
                       <HorizontalLine />
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="24px" mobile="20px" />
                       <Typography variant="body3" fontWeight="500">
                         Details
                       </Typography>
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="21px" mobile="20px" />
 
                       <div className={styles.text_dotted_wrapper}>
                         <TextDotted
                           fontWeightEndText="500"
-                          info={true}
-                          startTextColor={AppColor.transparentBlack}
-                          text="Revisions"
-                          textEnd="10"
-                        />
-                        <TextDotted
-                          fontWeightEndText="500"
-                          info={true}
-                          startTextColor={AppColor.transparentBlack}
-                          text="Source File"
-                          textEnd="2"
-                        />
-                        <TextDotted
-                          fontWeightEndText="500"
-                          info={true}
-                          startTextColor={AppColor.transparentBlack}
-                          text="High Resolution"
-                          textEnd="2"
-                        />
-                        <TextDotted
-                          fontWeightEndText="500"
-                          info={true}
+                          info={false}
                           startTextColor={AppColor.transparentBlack}
                           text="Delivery"
-                          textEnd="6 days"
+                          textEnd="10"
                         />
                       </div>
                       <DynamicPadding desktop="20px" mobile="15px" />
@@ -246,44 +239,28 @@ const OrdersCompleted = () => {
                         >
                           Specification
                         </Typography>
-                        <Typography
-                          fontWeight="500"
-                          color={AppColor.transparentBlack}
-                          variant="body4"
-                        >
-                          Documents
-                        </Typography>
                       </div>
 
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="27px" mobile="20px" />
                       <HorizontalLine />
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="23px" mobile="20px" />
 
                       <Typography variant="body3" fontWeight="500">
                         Subscription
                       </Typography>
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="27px" mobile="20px" />
                       <div className="flex_space_between">
-                        <ButtonChooseList
-                          buttonPadding="4px 13px"
-                          buttons={['Start', 'Pro', 'Ultimate']}
-                          callback={() => {}}
-                          gap="0px"
-                          initValue="Start"
+                        <SubscriptionList
+                          callback={(title: string) => {
+                            console.log(title)
+                          }}
                         />
 
                         <div className={styles.buy_wrapper}>
                           <AppColor.buy fill={AppColor.text} />
                         </div>
                       </div>
-                      <DynamicPadding desktop="20px" mobile="10px" />
-                      <div className="gap_5">
-                        <AppColor.queue fill={AppColor.orange} />
-                        <Typography variant="body4">
-                          Higher Priority Queue
-                        </Typography>
-                      </div>
-                      <DynamicPadding desktop="20px" mobile="10px" />
+                      <DynamicPadding desktop="18px" mobile="10px" />
                       <div className="gap_10">
                         <div className="gap_5">
                           <AppColor.moneyHummer />
@@ -294,35 +271,43 @@ const OrdersCompleted = () => {
                           <Typography variant="body4">10 days</Typography>
                         </div>
                       </div>
-                      <DynamicPadding desktop="20px" mobile="10px" />
-                      <Typography
-                        variant="body4"
-                        fontWeight="500"
-                        color={AppColor.transparentBlack}
+                      <DynamicPadding desktop="14px" mobile="10px" />
+                      <span
+                        className={styles.mission_btn}
+                        onClick={() => {
+                          setShowMissionModal(true)
+                        }}
                       >
-                        Missions
-                      </Typography>
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                        {' '}
+                        <Typography
+                          variant="body4"
+                          fontWeight="500"
+                          color={AppColor.transparentBlack}
+                        >
+                          Missions
+                        </Typography>
+                      </span>
+                      <DynamicPadding desktop="23px" mobile="20px" />
                       <HorizontalLine />
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="24px" mobile="20px" />
                       <Typography variant="body3" fontWeight="500">
                         Rewards
                       </Typography>
-                      <SizeBox height="10px" />
+                      <DynamicPadding desktop="20px" mobile="20px" />
 
                       <div className={styles.rewards_wrapper}>
                         <AppColor.reward10PTS />
                         <AppColor.reward30Xp />
                       </div>
 
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="25px" mobile="20px" />
                       <HorizontalLine />
-                      <DynamicPadding desktop="30px" mobile="20px" />
-                      <Typography variant="body3" fontWeight="500">
-                        Summary
-                      </Typography>
-                      <DynamicPadding desktop="30px" mobile="20px" />
+                      <DynamicPadding desktop="24px" mobile="20px" />
+
                       <div className={styles.text_dotted_wrapper}>
+                        <Typography variant="body3" fontWeight="500">
+                          Summary
+                        </Typography>
                         <TextDotted
                           fontWeightEndText="500"
                           startTextColor={AppColor.transparentBlack}
@@ -377,7 +362,6 @@ const OrdersCompleted = () => {
                         variant="body5"
                         color={AppColor.transparentBlack}
                         fontWeight="500"
-                        textTransform="uppercase"
                       >
                         Invoice
                       </Typography>
@@ -408,6 +392,14 @@ const OrdersCompleted = () => {
       </div>
 
       <Footer />
+
+      {showMissionModal && (
+        <MissionModal
+          onClose={() => {
+            setShowMissionModal(false)
+          }}
+        />
+      )}
     </div>
   )
 }
