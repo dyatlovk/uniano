5fdf5be5124c878c4d23bdbc9978cc9cfb004754
Kirill Dyatlov <kirillsoftware@proton.me>
2024-09-02 04:43:46 +0300

    update admin finance withdrawal modal
    * add amount step

--- src/pages/AdminPannel/pages/AdminWithdraw/AddWithrawalSteps/Amount/index.tsx
+++ src/pages/AdminPannel/pages/AdminWithdraw/AddWithrawalSteps/Amount/index.tsx
@@ -1,10 +1,44 @@
+import DynamicPadding from '@common/components/ui/DynamicPadding/index'
+import InputCommon from '@common/components/ui/inputs/InputCommon/index'
+import Typography from '@common/components/ui/Typography/Typography'
+import AppColor from '@common/styles/variables-static'
 import { useEffect } from 'react'
+import styles from './style.module.scss'
 
 const AmountStep = ({ onReady }: Props): JSX.Element => {
   useEffect(() => {
     onReady('Amount')
   })
-  return <>amount</>
+  return (
+    <div>
+      <div className={styles.update_amount}>
+        <InputCommon
+          backgroundColor={AppColor.white}
+          placeholder={'Enter Amount Manually'}
+          callback={() => {}}
+          padding={'15.5px 20px'}
+        />
+        <div className={styles.balance}>
+          <div className={styles.money_round}>
+            <AppColor.moneyWithdraw width={21} height={13} />
+          </div>
+          <div className={styles.balance_value}>
+            <Typography variant="body5">Withdrawal Balance</Typography>
+            <Typography color={AppColor.text}>$100</Typography>
+          </div>
+        </div>
+      </div>
+
+      <DynamicPadding desktop="20px" />
+
+      <Typography>
+        Max $95.47{' '}
+        <span style={{ color: AppColor.transparentBlack }}>
+          (payment system commission is 4.35% + 0.18$)
+        </span>
+      </Typography>
+    </div>
+  )
 }
 
 interface Props {
