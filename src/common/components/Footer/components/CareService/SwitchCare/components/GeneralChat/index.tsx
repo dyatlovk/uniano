bce0469afb662b99222a6072a336487b6bb7311f
Kirill Dyatlov <kirillsoftware@proton.me>
2024-11-17 04:42:46 +0200

    fix chat

--- src/common/components/Footer/components/CareService/SwitchCare/components/GeneralChat/index.tsx
+++ src/common/components/Footer/components/CareService/SwitchCare/components/GeneralChat/index.tsx
@@ -1,6 +1,8 @@
-import InputBarChat from '@common/components/ui/Chat/InputBar/index'
 import { MessagesDisplayUsers } from '@common/components/ui/Chat/MessagesDisplay/index'
 import styles from './style.module.scss'
+import { useRef } from 'react'
+import InputCommon from '@common/components/ui/inputs/InputCommon/index'
+import AppColor from '@common/styles/variables-static'
 
 const GeneralChat = () => {
   return (
@@ -49,6 +51,46 @@ const GeneralChat = () => {
       />
 
       <InputBarChat />
+    </div>
+  )
+}
+
+const InputBarChat = () => {
+  const inputFileRef = useRef(null)
+
+  const handleClickFile = () => {
+    if (inputFileRef.current) {
+      inputFileRef.current.click()
+    }
+  }
+
+  return (
+    <div className={styles.input_wrapper}>
+      <InputCommon
+        icon={
+          <div
+            onClick={handleClickFile}
+            className="cursor"
+            style={{ position: 'relative' }}
+          >
+            <input
+              ref={inputFileRef}
+              type="file"
+              className={`${styles.input_abs}`}
+            />
+            <AppColor.attach />
+          </div>
+        }
+        callback={() => {}}
+        placeholder="Message"
+        disableClose={true}
+        padding="15px 100px 15px 50px"
+        width="100%"
+      />
+      <div className={styles.right_abs}>
+        <AppColor.smile />
+        <AppColor.sendEmail />
+      </div>
     </div>
   )
 }
